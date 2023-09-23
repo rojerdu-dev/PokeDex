df3a1820a3079be544a2d49145641c2e4f42a448
Rojer Du <rojerdu.dev@gmail.com>
2023-09-23 15:58:58 -0700

    Add directory internal for pokeapi

---
+++ internal/pokeapi/pokeapi.go
@@ -0,0 +1,30 @@
+package pokeapi
+
+import (
+	"net/http"
+	"time"
+)
+
+const baseURL = "https://pokeapi.co/api/v2/"
+
+type Client struct {
+	httpClient http.Client
+}
+
+func NewClient() Client {
+	return Client{
+		httpClient: http.Client{
+			Timeout: time.Minute,
+		},
+	}
+}
+
+type LocationAreasResponse struct {
+	Count    int     `json:"count"`
+	Next     *string `json:"next"`
+	Previous *string `json:"previous"`
+	Results  []struct {
+		Name string `json:"name"`
+		URL  string `json:"url"`
+	} `json:"results"`
+}
