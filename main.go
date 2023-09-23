ed0d1e9dd449e9d99c9faeee544583a10089c8a1
Rojer Du <rojerdu.dev@gmail.com>
2023-09-23 14:33:13 -0700

    Add switch statement command

--- main.go
+++ main.go
@@ -20,8 +20,19 @@ func startRepl() {
 		inputText := scanner.Text()
 
 		cleaned := cleanInput(inputText)
+		if len(cleaned) == 0 {
+			continue
+		}
+		command := cleaned[0]
 
-		fmt.Println("right back at you:", cleaned)
+		switch command {
+		case "exit":
+			os.Exit(0)
+		default:
+			fmt.Println("invalid command")
+		}
+
+		//fmt.Println("right back at you:", cleaned)
 
 		if inputText == "exit" {
 			fmt.Println("sayonara")
